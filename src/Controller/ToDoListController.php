262102d443807ba711f10f9e8e65f5b9e825e03e
norcimilim.x <norcimilim@norcimilim.x>
2022-05-21 14:25:58 +0300

    Add twig, task entity, migration, crud methods

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,20 +2,63 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ToDoListController extends AbstractController
 {
     /**
-     * @Route("/to/do/list", name="app_to_do_list")
+     * @Route("/", name="to_do_list")
      */
-    public function index(): JsonResponse
+    public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ToDoListController.php',
-        ]);
+        return $this->render('list.html.twig');
+    }
+
+    /**
+     * @Route("/create", name="create_task", methods={"POST"})
+     */
+    public function create(Request $request)
+    {
+        $title = $request->request->get('title');
+
+        if (empty($title)) {
+            return $this->redirectToRoute('to_do_list');
+        }
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $task = new Task();
+        $task->setTitle($title);
+        $entityManager->persist($task);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('to_do_list');
+    }
+
+    /**
+     * @Route("/switch-status/{id}", name="switch-status")
+     */
+    public function switchStatus($id)
+    {
+        exit('Switch task status ' . $id);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete($id)
+    {
+        exit('Delete task ' . $id);
+    }
+
+    /**
+     * @Route("/update/{id}", name="update")
+     */
+    public function update($id)
+    {
+        exit('Update task ' . $id);
     }
 }
